94dca4ca5876255d9bca26c4c72ba805233ca946
luke-teo <yipengteo.luke@gmail.com>
2023-10-05 21:38:12 +0900

    feat: added app config, with providers for env and db

--- server/config/config.go
+++ server/config/config.go
@@ -1,1 +1,43 @@
 package config
+
+import (
+	"database/sql"
+	"path"
+	"runtime"
+
+	"github.com/teoyi/dimewise/config/provider"
+)
+
+type App struct {
+	env     *provider.EnvProvider
+	db      *sql.DB
+	rootDir string
+}
+
+func NewApp() *App {
+	a := &App{}
+	a.env = provider.NewEnvProvider()
+	a.setRootDir()
+
+	return a
+
+}
+
+func (a *App) EnvVars() *provider.EnvProvider {
+	if a.env == nil {
+		a.env = provider.NewEnvProvider()
+	}
+	return a.env
+}
+
+func (a *App) DB() *sql.DB {
+	if a.db == nil {
+		a.db = provider.NewDBProvider(a.env)
+	}
+	return a.db
+}
+
+func (a *App) setRootDir() {
+	_, b, _, _ := runtime.Caller(0)
+	a.rootDir = path.Join(path.Dir(b), "..")
+}
