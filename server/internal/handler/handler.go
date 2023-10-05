50c7c47de222dad0225d81485c0b3f56e59b8862
luke-teo <yipengteo.luke@gmail.com>
2023-10-05 21:39:08 +0900

    feat: added sample api request

--- server/internal/handler/handler.go
+++ server/internal/handler/handler.go
@@ -1,1 +1,22 @@
 package handler
+
+import (
+	"net/http"
+
+	"github.com/teoyi/dimewise/config"
+)
+
+type Handler struct {
+	App *config.App
+}
+
+func NewHandler(app *config.App) *Handler {
+	return &Handler{
+		App: app,
+	}
+}
+
+func (h *Handler) GetFunny(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("OK"))
+}
