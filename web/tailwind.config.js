97b68a84b04660a547e61d1c73227bd549acb3a3
luke-teo <yipengteo.luke@gmail.com>
2023-10-07 16:10:59 +0900

    feat: added theme and colors to tailwind config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],me: {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  me: {
     extend: {},
   },
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      background: "#2C3333",
+      foreground: "#395B64",
+      primary: "#A5C9CA",
+      secondary: "#E7F6F2",
+    },
+    fontFamily: {
+      go: ["go", "sans-serif"],
+    },
+  },
   plugins: [],
-}
-
+};
