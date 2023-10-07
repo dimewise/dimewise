68b68a2638da7398ab2ece69a69779830d2ce5fe
luke-teo <yipengteo.luke@gmail.com>
2023-10-07 14:14:06 +0900

    feat: added frontend to communicate with auth0

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,45 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
 function App() {
+  const { loginWithRedirect, isAuthenticated, logout, getAccessTokenSilently } =
+    useAuth0();
+
+  const showApiCall = () => {
+    getAccessTokenSilently().then((token) => {
+      fetch("http://localhost:3001/api/v1/test", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => alert(data.message));
+    });
+  };
+  console.log("Authenticated?: ", isAuthenticated);
+
   return (
-    <div className="bg-blue-500 text-red-200">
-      Hello World
+    <div className="flex flex-col items-center justify-center text-black ">
+      We testing out the login
+      <div>
+        {isAuthenticated ? (
+          <>
+            <button
+              onClick={() =>
+                logout({ logoutParams: { returnTo: window.location.origin } })
+              }
+            >
+              logout
+            </button>
+            <button onClick={() => showApiCall()}>show api call</button>
+          </>
+        ) : (
+          <button className="bg-blue-500" onClick={() => loginWithRedirect()}>
+            login
+          </button>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
