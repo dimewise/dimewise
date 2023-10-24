d5933c01263aa634258543a7acdc600d12f3fe3f
luke-teo <yipengteo.luke@gmail.com>
2023-10-24 21:50:04 +0900

    feat: created recent expenses table component

--- web/src/pages/dashboard/components/RecentExpenses/index.tsx
+++ web/src/pages/dashboard/components/RecentExpenses/index.tsx
@@ -1,84 +1,51 @@
 import React from "react"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Badge } from "@/components/ui/badge";
+import ListItem from "./components/ListItem";
+
+const data = [
+  {
+    id: "1",
+    title: "Din Tai Fung",
+    category: "Category 1",
+    amount: 4200
+  },
+  {
+    id: "2",
+    title: "Gucci",
+    category: "Category 2",
+    amount: 320000
+  },
+  {
+    id: "3",
+    title: "Arcade",
+    category: "Category 2",
+    amount: 1000
+  },
+  {
+    id: "4",
+    title: "Macarons",
+    category: "Category 1",
+    amount: 2300
+  },
+  {
+    id: "5",
+    title: "Suica",
+    category: "Category 3",
+    amount: 3000
+  }
+];
 
 const RecentExpenses: React.FC = () => {
   return (
-    <div className="space-y-8">
-      <div className="flex items-center">
-        <Avatar className="h-9 w-9">
-          <AvatarImage src="/avatars/01.png" alt="Avatar" />
-          <AvatarFallback>DTF</AvatarFallback>
-        </Avatar>
-        <div className="ml-4 space-y-1">
-          <div className="flex items-center space-x-3">
-            <p className="text-sm font-medium leading-none">Din Tai Fung</p>
-            <Badge variant="outline">Category 1</Badge>
-          </div>
-          <p className="text-sm text-muted-foreground">
-            Had lunch with baby at Toyosu
-          </p>
-        </div>
-        <div className="ml-auto font-medium">-¥4,200</div>
-      </div>
-      <div className="flex items-center">
-        <Avatar className="flex h-9 w-9 items-center justify-center space-y-0 border">
-          <AvatarImage src="/avatars/02.png" alt="Avatar" />
-          <AvatarFallback>G</AvatarFallback>
-        </Avatar>
-        <div className="ml-4 space-y-1">
-          <div className="flex items-center space-x-3">
-            <p className="text-sm font-medium leading-none">Gucci</p>
-            <Badge variant="outline">Category 2</Badge>
-          </div>
-          <p className="text-sm text-muted-foreground">Bought a new bag for work</p>
-        </div>
-        <div className="ml-auto font-medium">-¥320,000</div>
-      </div>
-      <div className="flex items-center">
-        <Avatar className="h-9 w-9">
-          <AvatarImage src="/avatars/03.png" alt="Avatar" />
-          <AvatarFallback>A</AvatarFallback>
-        </Avatar>
-        <div className="ml-4 space-y-1">
-          <div className="flex items-center space-x-3">
-            <p className="text-sm font-medium leading-none">Arcade</p>
-            <Badge variant="outline">Category 2</Badge>
-          </div>
-          <p className="text-sm text-muted-foreground">
-            Tekken 7 at Akihabara
-          </p>
-        </div>
-        <div className="ml-auto font-medium">-¥1,000</div>
-      </div>
-      <div className="flex items-center">
-        <Avatar className="h-9 w-9">
-          <AvatarImage src="/avatars/04.png" alt="Avatar" />
-          <AvatarFallback>M</AvatarFallback>
-        </Avatar>
-        <div className="ml-4 space-y-1">
-          <div className="flex items-center space-x-3">
-            <p className="text-sm font-medium leading-none">Macarons</p>
-            <Badge variant="outline">Category 1</Badge>
-          </div>
-          <p className="text-sm text-muted-foreground">Bought sweets from Afternoon Tea</p>
-        </div>
-        <div className="ml-auto font-medium">-¥2,300</div>
-      </div>
-      <div className="flex items-center">
-        <Avatar className="h-9 w-9">
-          <AvatarImage src="/avatars/05.png" alt="Avatar" />
-          <AvatarFallback>S</AvatarFallback>
-        </Avatar>
-        <div className="ml-4 space-y-1">
-          <div className="flex items-center space-x-3">
-            <p className="text-sm font-medium leading-none">Suica</p>
-            <Badge variant="outline">Category 3</Badge>
-          </div>
-          <p className="text-sm text-muted-foreground">Transport for train</p>
-        </div>
-        <div className="ml-auto font-medium">-¥3,000</div>
-      </div>
+    <div className="w-full flex flex-col space-y-4">
+      {data.map((expense) => (
+        <ListItem
+          key={expense.id}
+          id={expense.id}
+          title={expense.title}
+          category={expense.category}
+          amount={expense.amount}
+        />
+      ))}
     </div>
   )
 }
