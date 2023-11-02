7a9cc594217848a5523f5d83ff402653cace5444
luke-teo <yipengteo.luke@gmail.com>
2023-11-02 21:57:09 +0900

    feat: updated app layout

--- web/src/common/AppLayout/index.tsx
+++ web/src/common/AppLayout/index.tsx
@@ -1,73 +1,21 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import {
-  NavigationMenu, NavigationMenuItem, NavigationMenuList
-} from "@/components/ui/navigation-menu";
-import NavigationLink from "./components/NavigationLink";
-import { Button } from "@/components/ui/button";
-import { MdAdd } from "react-icons/md";
-import { IconContext } from "react-icons";
-import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import QuickAccess from "./components/QuickAccess";
+import AuthenticatedNavigation from "./components/AuthenticatedNavigation";
+import UnauthenticatedNavigation from "./components/UnauthenticatedNavigation";
 interface AppLayoutProps {
   children: React.ReactNode;
 }
 
 const AppLayout: React.FC<AppLayoutProps> = ({ children }: AppLayoutProps) => {
-  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  const { isAuthenticated } = useAuth0();
 
   return (
     <div className="w-full h-screen flex flex-col">
       <div className="flex flex-row h-16 w-full max-w-full justify-between items-center px-5 border-b shrink-0">
         {isAuthenticated ?
-          <>
-            <NavigationMenu>
-              <NavigationMenuList className="space-x-5">
-                <NavigationMenuItem>
-                  <NavigationLink to="/dashboard">
-                    Dashboard
-                  </NavigationLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <NavigationLink to="/history">
-                    History
-                  </NavigationLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <NavigationLink to="/wishlist">
-                    Wishlist
-                  </NavigationLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <NavigationLink to="/settings">
-                    Settings
-                  </NavigationLink>
-                </NavigationMenuItem>
-              </NavigationMenuList>
-            </NavigationMenu>
-          </>
+          <AuthenticatedNavigation />
           :
-          <>
-            <NavigationMenu>
-              <NavigationMenuList>
-                <NavigationMenuItem>
-                  <NavigationLink to="/">
-                    About
-                  </NavigationLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <NavigationLink to="/">
-                    Features
-                  </NavigationLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <NavigationLink to="/">
-                    Support/Help
-                  </NavigationLink>
-                </NavigationMenuItem>
-              </NavigationMenuList>
-            </NavigationMenu>
-            <Button onClick={() => loginWithRedirect()}>Login</Button>
-          </>
+          <UnauthenticatedNavigation />
         }
       </div>
       <div className="w-full flex-1 px-5 py-3">{children}</div>
