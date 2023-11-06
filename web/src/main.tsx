8c6a6c6683ab0d71e8f5846b78d075d427ba34a9
Luke Teo <yipengteo.luke@gmail.com>
2023-11-06 22:43:32 +0900

    fix: shifted auth0 provider to main to avoid race conditions

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -8,6 +8,7 @@ import History from "./pages/history/index.tsx";
 import Settings from "./pages/settings/index.tsx";
 import Wishlist from "./pages/wishlist/index.tsx";
 import AuthProvider from "./provider/AuthProvider/index.tsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const router = createBrowserRouter([
   {
@@ -44,8 +45,18 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+        redirect_uri: window.location.origin,
+      }}
+      cacheLocation="localstorage"
+    >
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
--- web/src/provider/AuthProvider/index.tsx
+++ web/src/provider/AuthProvider/index.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { useAuth0, Auth0Provider } from '@auth0/auth0-react';
+import { useAuth0 } from '@auth0/auth0-react';
 import { setDimewiseToken } from '../../api/custom-fetch';
 
 interface AuthProviderProps {
@@ -47,20 +47,9 @@ const AuthProvider: React.FC<AuthProviderProps> = ({ children }: AuthProviderPro
   console.log(accessToken);
 
   return (
-    <Auth0Provider
-      domain={import.meta.env.VITE_AUTH0_DOMAIN}
-      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-      authorizationParams={{
-        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
-        redirect_uri: window.location.origin,
-      }}
-      cacheLocation="localstorage"
-      useRefreshTokens={true}
-    >
-      <AuthContext.Provider value={{ accessToken }}>
-        {children}
-      </AuthContext.Provider >
-    </Auth0Provider>
+    <AuthContext.Provider value={{ accessToken }}>
+      {children}
+    </AuthContext.Provider >
   );
 };
 
