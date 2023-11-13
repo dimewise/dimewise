5194e9d72a400ca1071b2c6d38150af3ff537017
Luke Teo <yipengteo.luke@gmail.com>
2023-11-13 22:53:30 +0900

    feat: added user check middleware

--- server/api/middleware/user_check.go
+++ server/api/middleware/user_check.go
@@ -1,21 +1,46 @@
 package middleware
 
 import (
-	"log"
+	"errors"
+	"log/slog"
 	"net/http"
 
 	jwtmiddleware "github.com/auth0/go-jwt-middleware/v2"
 	"github.com/auth0/go-jwt-middleware/v2/validator"
-	"github.com/teoyi/dimewise/config"
+	"github.com/teoyi/dimewise/internal/domain"
+	"github.com/teoyi/dimewise/internal/repository"
+	lerrors "github.com/teoyi/dimewise/internal/util/errors"
 )
 
-func UserCheck(_ *config.App) func(next http.Handler) http.Handler {
+func UserCheck(re *repository.Repository) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
 			claims, _ := r.Context().Value(jwtmiddleware.ContextKey{}).(*validator.ValidatedClaims)
+			subject := claims.RegisteredClaims.Subject
+			account, err := re.Account.GetAccountByExternalID(subject)
+			if err != nil {
+				if errors.Is(err, lerrors.ErrResourceNotFound) {
+					slog.Debug("account not found, creating new one ")
+					// create new account
+					model := domain.CreateAccountByExternalID(subject)
+					createdAccount, createErr := re.Account.CreateAccountByModel(model)
+					if createErr != nil {
+						slog.Error("Error creating account", slog.Any("err", createErr))
+						http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+						return
+					}
 
-			// add checks here for accounts, if missing craete a record in db
-			log.Println(claims.RegisteredClaims.Subject)
+					slog.Debug("account created", slog.String("id", createdAccount.ID.String()))
+					next.ServeHTTP(w, r)
+				}
+
+				slog.Error("Error checking for account", slog.Any("err", err))
+				http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+				return
+			}
+
+			slog.Debug("account found", slog.String("id", account.ID.String()))
+
 			next.ServeHTTP(w, r)
 		}
 
