218b870121efd740cd53c08d44f6b35b94f5833c
Luke Teo <yipengteo.luke@gmail.com>
2023-12-30 18:16:48 +0900

    feat: added typescript eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,46 @@ module.exports = {
 		'prettier'
 	],
 	rules: {
+		'arrow-body-style': ['error', 'as-needed'],
+		'max-statements-per-line': ['warn', { max: 1 }],
+		'no-nested-ternary': 'warn',
+		'no-unneeded-ternary': 'warn',
+		'one-var-declaration-per-line': ['warn', 'always'],
+		'operator-assignment': ['warn', 'always'],
+		'operator-linebreak': ['warn', 'none'],
+		'max-depth': ['warn', 1],
+		'max-lines-per-function': ['warn', { max: 10, skipBlankLines: true, skipComments: true }],
+		'max-nested-callbacks': ['warn', 1],
+		'max-params': ['warn', 2],
+		'no-magic-numbers': [
+			'warn',
+			{
+				detectObjects: false,
+				enforceConst: true,
+				ignore: [-1, 0, 1, 2, 10, 100],
+				ignoreArrayIndexes: true
+			}
+		],
+		'arrow-parens': ['warn', 'as-needed'],
+		'block-spacing': ['warn', 'always'],
+		curly: ['warn', 'all'],
+		'no-multiple-empty-lines': ['warn', { max: 1, maxEOF: 1 }],
+		'no-unused-vars': 'off',
+		'@typescript-eslint/explicit-member-accessibility': [
+			'warn',
+			{
+				accessibility: 'no-public',
+				overrides: {
+					parameterProperties: 'explicit'
+				}
+			}
+		],
+		'@typescript-eslint/explicit-function-return-type': 'warn',
+		'@typescript-eslint/no-unused-vars': 'warn',
+		'@typescript-eslint/no-useless-constructor': 'warn',
+		'@typescript-eslint/no-empty-function': 'warn',
+
+		// Svelte
 		'svelte/no-target-blank': 'error',
 		'svelte/button-has-type': [
 			'error',
@@ -47,5 +87,6 @@ module.exports = {
 				parser: '@typescript-eslint/parser'
 			}
 		}
-	]
+	],
+	ignorePatterns: ['supabase']
 };
