55d4b4c899807ba995b85c93e2a7897788e82df4
Luke Teo <yipengteo.luke@gmail.com>
2023-12-30 18:00:34 +0900

    feat: added svelte based eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,26 @@ module.exports = {
 		'plugin:svelte/recommended',
 		'prettier'
 	],
+	rules: {
+		'svelte/no-target-blank': 'error',
+		'svelte/button-has-type': [
+			'error',
+			{
+				button: true,
+				submit: true,
+				reset: true
+			}
+		],
+		'svelte/no-ignored-unsubscribe': 'error',
+		'svelte/no-inline-styles': 'error',
+		'svelte/require-each-key': 'error',
+		'svelte/require-event-dispatcher-types': 'error',
+		'svelte/require-stores-init': 'error',
+		'svelte/valid-each-key': 'error',
+		'svelte/prefer-class-directive': 'error',
+		'svelte/shorthand-attribute': 'error',
+		'svelte/shorthand-directive': 'error'
+	},
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
 	parserOptions: {
