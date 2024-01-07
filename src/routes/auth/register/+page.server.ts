f46aadeaf861da8c9faddddf4adbcf7025089d20
Luke Teo <yipengteo.luke@gmail.com>
2024-01-07 16:37:50 +0900

    wip: register page

--- src/routes/auth/register/+page.server.ts
+++ src/routes/auth/register/+page.server.ts
@@ -3,6 +3,7 @@ import type { Actions } from './$types';
 import { AuthError } from '@supabase/supabase-js';
 import prisma from '$lib/server/prisma';
 import { Currencies } from '@prisma/client';
+import { HttpStatusCode } from '$lib/utils/HttpStatusCodes';
 
 export const actions: Actions = {
 	default: async ({ request, url, locals: { supabase } }) => {
@@ -13,20 +14,24 @@ export const actions: Actions = {
 		const { data, error } = await supabase.auth.signUp({
 			email,
 			password,
-			options: { emailRedirectTo: `${url.origin}/auth/callback` }
+			options: { emailRedirectTo: `${url.origin}/auth/callback` },
 		});
 
+		// TODO: add validaiton checks for email and password?
+
 		if (error) {
-			if (error instanceof AuthError && error.status >= 400 && error.status < 500) {
-				return fail(400, {
+			if (error instanceof AuthError && error.status != undefined && error.status >= 400 && error.status < 500) {
+				return fail(HttpStatusCode.BadRequest, {
+					email,
 					message: error.message,
-					success: false
+					success: false,
 				});
 			}
 
-			return fail(500, {
+			return fail(HttpStatusCode.InternalServerError, {
+				email,
 				message: error.message,
-				success: false
+				success: false,
 			});
 		}
 
@@ -35,17 +40,16 @@ export const actions: Actions = {
 				data: {
 					email: email,
 					authId: data?.user?.id as string,
-					defaultCurrency: Currencies.JPY
-				}
+					defaultCurrency: Currencies.JPY,
+				},
 			});
 		} catch (e) {
 			console.log(e);
 		}
 
 		return {
-			message:
-				'Should be good to go, please try signing in since email confirmation has been disabled',
-			success: true
+			message: 'Should be good to go, please try signing in since email confirmation has been disabled',
+			success: true,
 		};
-	}
+	},
 };
