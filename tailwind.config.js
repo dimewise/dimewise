cacd16a607c441199723c66a5eef795d96409bb0
Luke Teo <yipengteo.luke@gmail.com>
2024-01-07 16:37:20 +0900

    feat: removed skeleton, changed to daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,14 @@
 // @ts-check
-import { join } from 'path';
 
-// 1. Import the Skeleton plugin
-import { skeleton } from '@skeletonlabs/tw-plugin';
+import daisyui from 'daisyui';
+import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: 'class',
-	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
-	],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [
-		skeleton({
-			themes: { preset: ['skeleton'] }
-		})
-	]
+	plugins: [typography, daisyui],
 };
