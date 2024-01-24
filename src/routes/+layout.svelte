8242dc453e69df8aedf7aa89f47af283851f6ccf
Luke Teo <yipengteo.luke@gmail.com>
2024-01-24 22:49:29 +0900

    feat: added login session and protected route called dashboard

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,25 @@
 <script>
 	import Header from '$lib/components/Header.svelte';
+	import { onMount } from 'svelte';
 	import './styles.css';
+	import { invalidate } from '$app/navigation';
+
+	export let data;
+
+	let { supabase, session } = data;
+	$: ({ supabase, session } = data);
+
+	onMount(() => {
+		const {
+			data: { subscription },
+		} = supabase.auth.onAuthStateChange((_, _session) => {
+			if (_session?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return subscription.unsubscribe();
+	});
 </script>
 
 <div class="flex min-h-screen flex-col">
