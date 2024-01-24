af0079727003f034778aae1901081d335100b24a
Luke Teo <yipengteo.luke@gmail.com>
2024-01-24 22:49:08 +0900

    feat: added login logic and login page

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,12 +1,18 @@
-<script>
+<script lang="ts">
 	import Icon from '@iconify/svelte';
 	import { _ } from 'svelte-i18n';
+	import { superForm } from 'sveltekit-superforms/client';
+	import type { PageData } from './$types';
 
 	let socialButtons = [
 		{ icon: 'bxl:facebook-circle', text: 'Facebook', variant: '' },
 		{ icon: 'bxl:google', text: 'Google', variant: '' },
 		{ icon: 'bxl:apple', text: 'Apple', variant: '' },
 	];
+
+	export let data: PageData;
+	const { form, errors, enhance } = superForm(data.validateLoginForm, { resetForm: false, autoFocusOnError: 'detect' });
+	console.error(errors);
 </script>
 
 <svelte:head>
@@ -19,15 +25,15 @@
 		<div class="flex flex-col items-center justify-center">
 			<h2 class="my-0">{$_('page.login.title')}</h2>
 			<p>{$_('page.login.choose-preferred-login-method')}</p>
-			<form>
+			<form method="POST" action="?/login" use:enhance>
 				<div class="label">
 					<span class="label-text">{$_('page.login.email')}</span>
 				</div>
-				<input type="text" placeholder="name@example.com" class="input input-bordered mb-5 w-full" />
+				<input type="text" name="email" placeholder="name@example.com" class="input input-bordered mb-5 w-full" />
 				<div class="label">
 					<span class="label-text">{$_('page.login.password')}</span>
 				</div>
-				<input type="password" placeholder="password" class="input input-bordered mb-5 w-full" />
+				<input type="password" name="password" placeholder="password" class="input input-bordered mb-5 w-full" />
 				<button type="submit" class="btn btn-block">{$_('button.login')}</button>
 			</form>
 		</div>
@@ -43,7 +49,7 @@
 			{/each}
 		</div>
 
-		<div class="flex w-full flex-col items-start justify-center gap-1">
+		<div class="prose-sm flex w-full flex-col items-start justify-center gap-1">
 			<p class="my-1">
 				{$_('page.login.register-prompt')} <a href="/register">{$_('page.register')}</a>
 			</p>
