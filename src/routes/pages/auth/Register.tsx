4c0dd7acdfbd29c1c01222cdcf1fc6077fc234bf
luke-teo <67887422+teoyi@users.noreply.github.com>
2024-04-15 23:26:44 +0900

    feat: added simple layout

---
+++ src/routes/pages/auth/Register.tsx
@@ -0,0 +1,5 @@
+import type { FC, ReactElement } from "react";
+
+export const Register: FC = (): ReactElement => {
+	return <div>Register</div>;
+};
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,28 +2,36 @@ import { lazy } from "react";
 import type { RouteObject } from "react-router-dom";
 
 export const DimewisePaths = {
+	// Base Routes
 	Home: "/",
 	About: "/about",
 	PrivacyPolicy: "/privacy-policy",
 	TermsAndConditions: "/terms-and-conditions",
+	// Authentication Routes
+	Login: "/auth/login",
+	Register: "/auth/register",
+	// Authenticated Routes
 	Dashboard: "/dashboard",
 };
 
-// routes declaration
-const Home = lazy(() => import("./pages/home/Home").then((module) => ({ default: module.Home })));
-const About = lazy(() => import("./pages/about/About").then((module) => ({ default: module.About })));
+const Home = lazy(() => import("./pages/home/Home").then((m) => ({ default: m.Home })));
+const About = lazy(() => import("./pages/about/About").then((m) => ({ default: m.About })));
 const PrivacyPolicy = lazy(() =>
-	import("./pages/privacy-policy/PrivacyPolicy").then((module) => ({ default: module.PrivacyPolicy })),
+	import("./pages/privacy-policy/PrivacyPolicy").then((m) => ({ default: m.PrivacyPolicy })),
 );
 const TermsAndConditions = lazy(() =>
-	import("./pages/terms-and-conditions/TermsAndConditions").then((module) => ({ default: module.TermsAndConditions })),
+	import("./pages/terms-and-conditions/TermsAndConditions").then((m) => ({ default: m.TermsAndConditions })),
 );
-const Dashboard = lazy(() => import("./pages/dashboard/Dashboard").then((module) => ({ default: module.Dashboard })));
+const Login = lazy(() => import("./pages/auth/Login").then((m) => ({ default: m.Login })));
+const Register = lazy(() => import("./pages/auth/Register").then((m) => ({ default: m.Register })));
+const Dashboard = lazy(() => import("./pages/dashboard/Dashboard").then((m) => ({ default: m.Dashboard })));
 
 export const DimewiseRoutes: RouteObject[] = [
 	{ path: DimewisePaths.Home, element: <Home /> },
 	{ path: DimewisePaths.About, element: <About /> },
 	{ path: DimewisePaths.PrivacyPolicy, element: <PrivacyPolicy /> },
 	{ path: DimewisePaths.TermsAndConditions, element: <TermsAndConditions /> },
+	{ path: DimewisePaths.Login, element: <Login /> },
+	{ path: DimewisePaths.Register, element: <Register /> },
 	{ path: DimewisePaths.Dashboard, element: <Dashboard /> },
 ];
