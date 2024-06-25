10bddcf9f42314265c52eb900a0b9dffc4012da3
luke-teo <67887422+teoyi@users.noreply.github.com>
2024-06-25 23:39:26 +0900

    feat(client): dashboard skeleton, need to revisit

--- client/src/components/layout/PrivateLayout.tsx
+++ client/src/components/layout/PrivateLayout.tsx
@@ -1,23 +1,93 @@
-import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { Routes } from "../../Routes";
 import { useAuth } from "../../hooks/useAuth";
 
 export const PrivateLayout = () => {
-	const { session: authSession } = useAuth();
+	const { session: authSession, logout } = useAuth();
 	const location = useLocation();
+	const navigate = useNavigate();
 
-	if (!authSession) {
-		return (
-			<Navigate
-				to={Routes.Login}
-				state={{ from: location }}
-				replace
-			/>
-		);
-	}
+	// TODO: find a better way to handle automatic re-route
+	useEffect(() => {
+		if (!authSession) {
+			console.log("redirecting");
+			return navigate(Routes.Login, { state: { from: location }, replace: true });
+		}
+	}, [navigate, location, authSession]);
+
+	const handleLogout = () => {
+		logout();
+	};
+
 	return (
-		<div className="bg-green-500">
-			<Outlet />
+		<div className="flex h-full w-full">
+			<div className="drawer lg:drawer-open">
+				<input
+					id="my-drawer-2"
+					type="checkbox"
+					className="drawer-toggle"
+				/>
+				<div className="drawer-content flex flex-col items-center justify-center">
+					{/* Page content here */}
+					<label
+						htmlFor="my-drawer-2"
+						className="btn btn-primary drawer-button lg:hidden"
+					>
+						Open drawer
+					</label>
+					<main>
+						<Outlet />
+					</main>
+				</div>
+				<div className="drawer-side">
+					<label
+						htmlFor="my-drawer-2"
+						aria-label="close sidebar"
+						className="drawer-overlay"
+					/>
+					<ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+						{/* Sidebar content here */}
+						<MenuList />
+						<div className="divider" />
+						<AccountMenuList handleLogout={handleLogout} />
+					</ul>
+				</div>
+			</div>
 		</div>
+	);
+};
+
+const MenuList = () => {
+	return (
+		<>
+			<li>
+				<a href="/">Dashboard</a>
+			</li>
+			<li>
+				<a href="/">Budget</a>
+			</li>
+		</>
+	);
+};
+
+interface AccountMenuListProps {
+	handleLogout: () => void;
+}
+const AccountMenuList = ({ handleLogout }: AccountMenuListProps) => {
+	return (
+		<>
+			<li>
+				<a href="/">Account Settings</a>
+			</li>
+			<li>
+				<button
+					type="button"
+					onClick={handleLogout}
+				>
+					Logout
+				</button>
+			</li>
+		</>
 	);
 };
--- client/src/components/layout/PublicLayout.tsx
+++ client/src/components/layout/PublicLayout.tsx
@@ -4,52 +4,50 @@ import { MenuIcon } from "../../assets/Icons/MenuIcon";
 
 export const PublicLayout = () => {
 	return (
-		<>
-			<div className="drawer drawer-end h-full">
-				<input
-					id="nav-drawer"
-					type="checkbox"
-					className="drawer-toggle"
-				/>
-				<div className="drawer-content flex flex-col justify-start items-center">
-					{/* Navbar */}
-					<nav className="navbar w-full max-w-7xl h-navbar flex justify-between items-center px-5 flex-none">
-						<h1 className="font-black text-2xl">
-							Dimewise<span className="text-primary">.</span>
-						</h1>
-						<div className="flex-none lg:hidden">
-							<label
-								htmlFor="nav-drawer"
-								aria-label="open sidebar"
-								className="btn btn-square btn-ghost"
-							>
-								<MenuIcon />
-							</label>
-						</div>
-						<div className="flex-none hidden lg:block">
-							<ul className="menu menu-horizontal gap-x-3">
-								{/* Navbar menu content here */}
-								<MenuList />
-							</ul>
-						</div>
-					</nav>
-					<main className="flex-1 w-full h-full flex justify-center items-start">
-						<Outlet />
-					</main>
-				</div>
-				<nav className="drawer-side">
-					<label
-						htmlFor="nav-drawer"
-						aria-label="close sidebar"
-						className="drawer-overlay"
-					/>
-					<ul className="menu p-4 w-80 min-h-full bg-base-200">
-						{/* Sidebar content here */}
-						<MenuList />
-					</ul>
+		<div className="drawer drawer-end h-full">
+			<input
+				id="nav-drawer"
+				type="checkbox"
+				className="drawer-toggle"
+			/>
+			<div className="drawer-content flex flex-col justify-start items-center">
+				{/* Navbar */}
+				<nav className="navbar w-full max-w-7xl h-navbar flex justify-between items-center px-5 flex-none">
+					<h1 className="font-black text-2xl">
+						Dimewise<span className="text-primary">.</span>
+					</h1>
+					<div className="flex-none lg:hidden">
+						<label
+							htmlFor="nav-drawer"
+							aria-label="open sidebar"
+							className="btn btn-square btn-ghost"
+						>
+							<MenuIcon />
+						</label>
+					</div>
+					<div className="flex-none hidden lg:block">
+						<ul className="menu menu-horizontal gap-x-3">
+							{/* Navbar menu content here */}
+							<MenuList />
+						</ul>
+					</div>
 				</nav>
+				<main className="flex-1 w-full h-full flex justify-center items-start">
+					<Outlet />
+				</main>
 			</div>
-		</>
+			<nav className="drawer-side">
+				<label
+					htmlFor="nav-drawer"
+					aria-label="close sidebar"
+					className="drawer-overlay"
+				/>
+				<ul className="menu p-4 w-80 min-h-full bg-base-200">
+					{/* Sidebar content here */}
+					<MenuList />
+				</ul>
+			</nav>
+		</div>
 	);
 };
 
