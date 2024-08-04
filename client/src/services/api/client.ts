f9c1a7e5e8c74cf0a30c11e6f94eaf509fd64858
luke-teo <67887422+teoyi@users.noreply.github.com>
2024-08-04 14:25:59 +0900

    feat: added rtk query + update packages to latest

---
+++ client/src/services/api/client.ts
@@ -0,0 +1,14 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const baseApiV1 = createApi({
+    baseQuery: fetchBaseQuery({
+        baseUrl: "/api/v1",
+        credentials: "include",
+        prepareHeaders: (headers) => {
+            headers.set("Accept", "application/json");
+            headers.set("Content-Type", "application/json");
+            return headers;
+        },
+    }),
+    endpoints: () => ({}),
+});
