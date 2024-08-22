8217a67d5a8dea3c4051e06c52fd7791a9960da9
Halvor <halvorkm@pm.me>
2024-08-22 12:41:32 +0900

    Add Dockerfile

--- server/app/settings.py
+++ server/app/settings.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     SUPABASE_KEY: str
     SUPABASE_JWT: str
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file="../.env", env_file_encoding="utf-8")
 
 
 settings = Settings()
