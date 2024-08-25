ec78a3bb12b7d2260bb4095e88e15b6094d0afe5
luke-teo <67887422+teoyi@users.noreply.github.com>
2024-08-25 21:33:43 +0900

    feat: added pyproject

--- server/app/main.py
+++ server/app/main.py
@@ -1,14 +1,15 @@
 from fastapi import APIRouter, Depends, FastAPI, Request
 from models import Category, Expense
-from settings import settings
 from sqlmodel import Session, create_engine, select
 from utils.jwt import JWTBearer
 
+from app.settings import Settings
+
 app = FastAPI()
 pub = APIRouter(prefix="/api/v1")
-prt = APIRouter(prefix="/api/v1", dependencies=[Depends(JWTBearer(settings.JWT_TOKEN))])
+prt = APIRouter(prefix="/api/v1", dependencies=[Depends(JWTBearer(Settings.JWT_TOKEN))])
 
-engine = create_engine(settings.DB_URL, echo=True)
+engine = create_engine(Settings.DB_URL, echo=True)
 
 
 @pub.get("/")
@@ -16,23 +17,19 @@ def root():
     return "Wow"
 
 
-@prt.get("/categories", response_model=list[Category])
+@prt.get("/categories", response_model="list[Category]")
 def get_categories(request: Request):
     creds = request.state.creds
     with Session(engine) as session:
-        categories = session.exec(
-            select(Category).where(Category.userId == creds["sub"])
-        ).all()
+        categories = session.exec(select(Category).where(Category.userId == creds["sub"])).all()
         return categories
 
 
-@prt.get("/expenses/recent", response_model=list[Expense])
+@prt.get("/expenses/recent", response_model="list[Expense]")
 def get_recent_expenses(request: Request):
     creds = request.state.creds
     with Session(engine) as session:
-        expenses = session.exec(
-            select(Expense).where(Expense.userId == creds["sub"])
-        ).all()
+        expenses = session.exec(select(Expense).where(Expense.userId == creds["sub"])).all()
         return expenses
 
 
