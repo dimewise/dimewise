b9189b3e257c404875bdfcd073b48487398cb573
luke-teo <67887422+teoyi@users.noreply.github.com>
2024-08-29 21:42:16 +0900

    feat: installed mui v6 + icons + fontsource inter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,10 @@ import "./lib/locale/i18n";
 import { AuthProvider } from "./components/context/AuthContext";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import "@fontsource/inter/300.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/700.css";
 
 export const App = () => {
 	return (
