e18e046e5a9ff507e9a6c3ceb735cb2d811fd650
luke-teo <67887422+teoyi@users.noreply.github.com>
2024-09-01 13:20:57 +0900

    fix: light mode issue in highlights

--- client/src/components/Home/Highlights.tsx
+++ client/src/components/Home/Highlights.tsx
@@ -48,87 +48,73 @@ const items = [
 
 export const Highlights = () => {
 	return (
-		<Box
-			id="highlights"
+		<Container
 			sx={{
 				pt: { xs: 4, sm: 12 },
 				pb: { xs: 8, sm: 16 },
-				color: "white",
-				bgcolor: "grey.900",
+				position: "relative",
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+				gap: { xs: 3, sm: 6 },
 			}}
 		>
-			<Container
+			<Box
 				sx={{
-					position: "relative",
-					display: "flex",
-					flexDirection: "column",
-					alignItems: "center",
-					gap: { xs: 3, sm: 6 },
+					width: { sm: "100%", md: "60%" },
+					textAlign: { sm: "left", md: "center" },
 				}}
 			>
-				<Box
-					sx={{
-						width: { sm: "100%", md: "60%" },
-						textAlign: { sm: "left", md: "center" },
-					}}
+				<Typography
+					component="h2"
+					variant="h4"
+					gutterBottom
+					sx={{ color: "text.primary" }}
 				>
-					<Typography
-						component="h2"
-						variant="h4"
-						gutterBottom
-					>
-						Highlights
-					</Typography>
-					<Typography
-						variant="body1"
-						sx={{ color: "grey.400" }}
-					>
-						Explore why our product stands out: adaptability, durability, user-friendly design, and innovation. Enjoy
-						reliable customer support and precision in every detail.
-					</Typography>
-				</Box>
-				<Grid
-					container
-					spacing={2}
+					Highlights
+				</Typography>
+				<Typography
+					variant="body1"
+					sx={{ color: "text.secondary" }}
 				>
-					{items.map((item, index) => (
-						<Grid
-							size={{ xs: 12, sm: 6, md: 4 }}
-							key={index}
+					Explore why our product stands out: adaptability, durability, user-friendly design, and innovation. Enjoy
+					reliable customer support and precision in every detail.
+				</Typography>
+			</Box>
+			<Grid
+				container
+				spacing={2}
+			>
+				{items.map((item, index) => (
+					<Grid
+						size={{ xs: 12, sm: 6, md: 4 }}
+						key={index}
+					>
+						<Card
+							variant="outlined"
+							sx={{
+								display: "flex",
+								flexDirection: "column",
+								flexGrow: 1,
+								p: 3,
+								height: "100%",
+								color: "text.secondary",
+							}}
 						>
-							<Stack
-								direction="column"
-								component={Card}
-								spacing={1}
-								useFlexGap
-								sx={{
-									color: "inherit",
-									p: 3,
-									height: "100%",
-									borderColor: "hsla(220, 25%, 25%, 0.3)",
-									backgroundColor: "grey.800",
-								}}
-							>
-								<Box sx={{ opacity: "50%" }}>{item.icon}</Box>
-								<div>
-									<Typography
-										gutterBottom
-										sx={{ fontWeight: "medium" }}
-									>
-										{item.title}
-									</Typography>
-									<Typography
-										variant="body2"
-										sx={{ color: "grey.400" }}
-									>
-										{item.description}
-									</Typography>
-								</div>
-							</Stack>
-						</Grid>
-					))}
-				</Grid>
-			</Container>
-		</Box>
+							<Box sx={{ opacity: "50%" }}>{item.icon}</Box>
+							<Box>
+								<Typography
+									gutterBottom
+									sx={{ fontWeight: "medium" }}
+								>
+									{item.title}
+								</Typography>
+								<Typography variant="body2">{item.description}</Typography>
+							</Box>
+						</Card>
+					</Grid>
+				))}
+			</Grid>
+		</Container>
 	);
 };
