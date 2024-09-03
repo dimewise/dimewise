3ff5c65386d3d3a309daa83819495817b442497f
luke-teo <67887422+teoyi@users.noreply.github.com>
2024-09-03 21:46:15 +0900

    fix: removed unnecessasry files

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -1,7 +1,5 @@
 import { type RefObject, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { AccountSetting } from "../components/AccountSetting";
-import { CategoriesSetting } from "../components/CategoriesSetting";
 import { CategoryFormModal } from "../components/modal/CategoryFormModal";
 import { ConfirmLogoutModal } from "../components/modal/ConfirmLogoutModal";
 import { DeleteCategoryModal } from "../components/modal/DeleteCategoryModal";
@@ -13,31 +11,17 @@ export const Settings = () => {
 	const categoryFormModalRef = useRef<HTMLDialogElement>(null);
 	const deleteCategoryModalRef = useRef<HTMLDialogElement>(null);
 
-	const handleShowModal = (modalRef: RefObject<HTMLDialogElement>) => {
-		modalRef.current?.showModal();
-	};
 	const handleCloseModal = (modalRef: RefObject<HTMLDialogElement>) => {
 		modalRef.current?.close();
 	};
 
-	const handleShowConfirmLogoutModal = () => handleShowModal(confirmLogoutModalRef);
 	const handleCloseConfirmLogoutModal = () => handleCloseModal(confirmLogoutModalRef);
 
-	const handleShowCategoryFormModal = () => handleShowModal(categoryFormModalRef);
 	const handleCloseCategoryFormModal = () => handleCloseModal(categoryFormModalRef);
-
-	const handleShowDeleteCategoryModal = () => handleShowModal(deleteCategoryModalRef);
-	const handleCloseDeleteCategoryModal = () => handleCloseModal(deleteCategoryModalRef);
 
 	return (
 		<>
 			<h1 className="sticky top-0 bg-white z-10 py-2">{t("nav.private.settings")}</h1>
-			<CategoriesSetting
-				handleShowCategoryFormModal={handleShowCategoryFormModal}
-				handleShowDeleteCategoryModal={handleShowDeleteCategoryModal}
-				setSelectedCategoryId={setSelectedCategoryId}
-			/>
-			<AccountSetting handleShowConfirmLogoutModal={handleShowConfirmLogoutModal} />
 			<CategoryFormModal
 				ref={categoryFormModalRef}
 				handleClose={handleCloseCategoryFormModal}
@@ -50,7 +34,7 @@ export const Settings = () => {
 			/>
 			<DeleteCategoryModal
 				ref={deleteCategoryModalRef}
-				handleClose={handleCloseDeleteCategoryModal}
+				handleClose={() => {}}
 				selectedCategoryId={selectedCategoryId}
 				setSelectedCategoryId={setSelectedCategoryId}
 			/>
