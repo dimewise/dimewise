777e0db7f4a947008da43ce9708d27ca0fa7a5cd
luke-teo <67887422+teoyi@users.noreply.github.com>
2024-09-04 17:44:07 +0900

    fix: removed unused portion of settings

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -1,43 +1,23 @@
-import { type RefObject, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { CategoryFormModal } from "../components/modal/CategoryFormModal";
-import { ConfirmLogoutModal } from "../components/modal/ConfirmLogoutModal";
-import { DeleteCategoryModal } from "../components/modal/DeleteCategoryModal";
+import { Box, Stack, Typography } from "@mui/material";
 
 export const Settings = () => {
 	const { t } = useTranslation();
-	const [selectedCategoryId, setSelectedCategoryId] = useState<string | null>(null);
-	const confirmLogoutModalRef = useRef<HTMLDialogElement>(null);
-	const categoryFormModalRef = useRef<HTMLDialogElement>(null);
-	const deleteCategoryModalRef = useRef<HTMLDialogElement>(null);
-
-	const handleCloseModal = (modalRef: RefObject<HTMLDialogElement>) => {
-		modalRef.current?.close();
-	};
-
-	const handleCloseConfirmLogoutModal = () => handleCloseModal(confirmLogoutModalRef);
-
-	const handleCloseCategoryFormModal = () => handleCloseModal(categoryFormModalRef);
 
 	return (
-		<>
-			<h1 className="sticky top-0 bg-white z-10 py-2">{t("nav.private.settings")}</h1>
-			<CategoryFormModal
-				ref={categoryFormModalRef}
-				handleClose={handleCloseCategoryFormModal}
-				selectedCategoryId={selectedCategoryId}
-				setSelectedCategoryId={setSelectedCategoryId}
-			/>
-			<ConfirmLogoutModal
-				ref={confirmLogoutModalRef}
-				handleClose={handleCloseConfirmLogoutModal}
-			/>
-			<DeleteCategoryModal
-				ref={deleteCategoryModalRef}
-				handleClose={() => {}}
-				selectedCategoryId={selectedCategoryId}
-				setSelectedCategoryId={setSelectedCategoryId}
-			/>
-		</>
+		<Box sx={{ width: "100%", maxWidth: { sm: "100%", md: "1700px" } }}>
+			<Stack
+				sx={{ justifyContent: "space-between" }}
+				direction="row"
+			>
+				<Typography
+					component="h2"
+					variant="h6"
+					sx={{ mb: 2 }}
+				>
+					{t("nav.private.transactions")}
+				</Typography>
+			</Stack>
+		</Box>
 	);
 };
