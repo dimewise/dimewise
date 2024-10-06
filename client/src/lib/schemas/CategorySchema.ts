be0388b54414ff4b5baa59d77f1f4fbc2c04f886
luke-teo <67887422+teoyi@users.noreply.github.com>
2024-10-06 18:04:38 +0900

    fix: added restriction that a budget has to be more than 0

--- client/src/lib/schemas/CategorySchema.ts
+++ client/src/lib/schemas/CategorySchema.ts
@@ -1,8 +1,8 @@
 import * as yup from "yup";
 
 export const CategorySchema = yup.object({
-	name: yup.string().max(255, "Name cannot be longer than 255 characters").required("Name is required"),
-	budget: yup.number().required("Budget is required"),
+  name: yup.string().max(255, "Name cannot be longer than 255 characters").required("Name is required"),
+  budget: yup.number().required("Budget is required").min(1, "Budget has to be more than 0"),
 });
 
-export interface CategorySchemaType extends yup.InferType<typeof CategorySchema> {}
+export interface CategorySchemaType extends yup.InferType<typeof CategorySchema> { }
