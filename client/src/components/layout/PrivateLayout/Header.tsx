eabb7bcf7dde338b7cf396482c92df1ed529f3e5
luke-teo <67887422+teoyi@users.noreply.github.com>
2024-12-04 21:03:03 +0900

    fix: removed unnecessary components in dashboard navbar

--- client/src/components/layout/PrivateLayout/Header.tsx
+++ client/src/components/layout/PrivateLayout/Header.tsx
@@ -1,22 +1,7 @@
-import NotificationsRoundedIcon from "@mui/icons-material/NotificationsRounded";
 import Stack from "@mui/material/Stack";
-import { useDispatch, useSelector } from "react-redux";
-import type { RootState } from "../../../store";
-import { toggleMode } from "../../../store/themeSlice";
-import { CustomDatePicker } from "../../Dashboard/CustomDatePicker";
 import { NavbarBreadcrumbs } from "../../Dashboard/NavbarBreadcrumbs";
-import { Search } from "../../Dashboard/Search";
-import { ToggleColorMode } from "../../Home/ToggleMode";
-import { MenuButton } from "../../MenuButton";
 
 export const Header = () => {
-	const dispatch = useDispatch();
-	const mode = useSelector((state: RootState) => state.theme.mode);
-
-	const handleToggleMode = () => {
-		dispatch(toggleMode());
-	};
-
 	return (
 		<Stack
 			direction="row"
@@ -32,23 +17,6 @@ export const Header = () => {
 			spacing={2}
 		>
 			<NavbarBreadcrumbs />
-			<Stack
-				direction="row"
-				sx={{ gap: 1 }}
-			>
-				<Search />
-				<CustomDatePicker />
-				<MenuButton
-					showBadge
-					aria-label="Open notifications"
-				>
-					<NotificationsRoundedIcon />
-				</MenuButton>
-				<ToggleColorMode
-					mode={mode}
-					toggleColorMode={handleToggleMode}
-				/>
-			</Stack>
 		</Stack>
 	);
 };
