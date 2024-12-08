b1180942f8f12d3cc0d551c26039f66bef00627e
luke-teo <67887422+teoyi@users.noreply.github.com>
2024-12-08 15:05:04 +0900

    feat: added ja and implemented i18n for error translations

--- client/src/lib/schemas/CategorySchema.ts
+++ client/src/lib/schemas/CategorySchema.ts
@@ -1,16 +1,17 @@
 import { z } from "zod";
+import i18n from "../locale/i18n";
 
 export const CategorySchema = z.object({
-	name: z
-		.string({
-			required_error: "categories.form.field_name.validate_required",
-		})
-		.max(255, "categories.form.field_name.validate_maximum_length"),
-	budget: z
-		.number({
-			required_error: "categories.form.field_budget.validate_required",
-		})
-		.min(1, "categories.form.field_budget.validate_minimum_value"),
+  name: z
+    .string({
+      required_error: i18n.t("categories.form.field_name.validate_required"),
+    })
+    .max(255, i18n.t("categories.form.field_name.validate_maximum_length")),
+  budget: z
+    .number({
+      required_error: i18n.t("categories.form.field_budget.validate_required"),
+    })
+    .min(1, i18n.t("categories.form.field_budget.validate_minimum_value")),
 });
 
 export type CategoryFormData = z.infer<typeof CategorySchema>;
