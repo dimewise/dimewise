c7eae4a0c7c12027074017b1de5c53ef3102152a
luke-teo <67887422+teoyi@users.noreply.github.com>
2024-12-08 15:05:04 +0900

    feat: added ja and implemented i18n for error translations

--- client/src/lib/schemas/ForgotPasswordSchema.ts
+++ client/src/lib/schemas/ForgotPasswordSchema.ts
@@ -1,13 +1,14 @@
 import { z } from "zod";
+import i18n from "../locale/i18n";
 
 export const ForgotPasswordSchema = z.object({
-	email: z
-		.string({
-			required_error: "auth.form.field_email.validate_required",
-		})
-		.email({
-			message: "auth.form.field_email.validate_not_an_email",
-		}),
+  email: z
+    .string({
+      required_error: i18n.t("auth.form.field_email.validate_required"),
+    })
+    .email({
+      message: i18n.t("auth.form.field_email.validate_not_an_email"),
+    }),
 });
 
 export type ForgotPasswordFormData = z.infer<typeof ForgotPasswordSchema>;
