b2af171ddde112a555e75d3ca2c73289addddb3a
luke-teo <67887422+teoyi@users.noreply.github.com>
2024-12-08 14:33:52 +0900

    feat: added ja translation

--- client/src/lib/locale/i18n.ts
+++ client/src/lib/locale/i18n.ts
@@ -2,24 +2,26 @@ import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 import transEn from "./lang/en.json";
+import transJa from "./lang/ja.json";
 
 export const defaultNS = "translation";
 export const resources = {
-	en: { translation: transEn },
+  en: { translation: transEn },
+  ja: { translation: transJa },
 } as const;
 
 i18n
-	.use(LanguageDetector)
-	.use(initReactI18next)
-	.init({
-		debug: import.meta.env.VITE_APP_ENV === "local",
-		fallbackLng: "en",
-		defaultNS,
-		ns: ["translation"],
-		resources,
-		interpolation: {
-			escapeValue: false,
-		},
-	});
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    debug: import.meta.env.VITE_APP_ENV === "local",
+    fallbackLng: "en",
+    defaultNS,
+    ns: ["translation"],
+    resources,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
 export default i18n;
