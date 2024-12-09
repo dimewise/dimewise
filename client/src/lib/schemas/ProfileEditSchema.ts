318ed3736d4e60f1658fd8a459d794fb04eb38c3
luke-teo <67887422+teoyi@users.noreply.github.com>
2024-12-09 21:54:05 +0900

    fix: onSubmit not working for profile edit

--- client/src/lib/schemas/ProfileEditSchema.ts
+++ client/src/lib/schemas/ProfileEditSchema.ts
@@ -6,7 +6,6 @@ export const ProfileEditSchema = z.object({
       required_error: "settings.profile.form.field_name.validate_required",
     })
     .max(255, "settings.profile.form.field_name.validate_maximum_length"),
-  avatar_url: z.string(),
 });
 
 export type ProfileEditFormData = z.infer<typeof ProfileEditSchema>;
--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from "react-i18next";
 import { ProfileEditor } from "../components/Settings/ProfileEditor";
 import { ThemeSelector } from "../components/Settings/ThemeSelector";
+import { Toast } from "../components/Toast";
 import { PageNavbar } from "../components/layout/PrivateLayout/PageNavbar";
 
 export const Settings = () => {
@@ -11,6 +12,7 @@ export const Settings = () => {
       <PageNavbar title={t("nav.private.settings")} />
       <ProfileEditor />
       <ThemeSelector />
+      <Toast />
     </>
   );
 };
