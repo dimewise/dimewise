5ca050ffd83d2b45422ce8c8397b78e1236e6d33
luke-teo <67887422+teoyi@users.noreply.github.com>
2024-12-09 21:59:36 +0900

    fix: added missing transaltions

--- client/src/components/Settings/ThemeSelector.tsx
+++ client/src/components/Settings/ThemeSelector.tsx
@@ -1,9 +1,11 @@
 import { Box, Divider, Switch, Typography } from "@mui/material";
+import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
 import type { RootState } from "../../store";
 import { toggleMode } from "../../store/themeSlice";
 
 export const ThemeSelector = () => {
+  const { t } = useTranslation();
   const dispatch = useDispatch();
   const mode = useSelector((state: RootState) => state.theme.mode);
 
@@ -13,10 +15,10 @@ export const ThemeSelector = () => {
 
   return (
     <Box sx={{ display: "flex", flexDirection: "column", alignItems: "start", justifyContent: "center" }}>
-      <Typography variant="h5">Theme</Typography>
+      <Typography variant="h5">{t("settings.theme.title")}</Typography>
       <Divider />
       <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", gap: 2 }}>
-        <Typography>Dark Mode</Typography>
+        <Typography>{t("settings.theme.dark-mode")}</Typography>
         <Switch
           checked={mode === "dark"}
           onChange={handleToggleMode}
