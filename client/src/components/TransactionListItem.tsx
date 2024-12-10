155e485cee6422131c7c359447a0a914ba0485d4
luke-teo <67887422+teoyi@users.noreply.github.com>
2024-12-10 21:43:32 +0900

    feat: added currency conversion display to recent transactions widget

--- client/src/components/TransactionListItem.tsx
+++ client/src/components/TransactionListItem.tsx
@@ -1,7 +1,8 @@
 import { ListItem, ListItemButton, ListItemText, Typography } from "@mui/material";
 import { DateTime } from "luxon";
 import { useState } from "react";
-import type { Expense } from "../services/api/v1";
+import { formatCurrencyValueToLocale, parseCurrencyEnum } from "../lib/util/currency";
+import { type Expense, useApiV1UserMeDetailGetMeDetailQuery } from "../services/api/v1";
 import { TransactionDetails } from "./Transactions/TransactionDetails";
 
 interface Props {
@@ -9,10 +10,18 @@ interface Props {
   disallowOpen?: boolean;
 }
 
-// TODO: switch to use account currency instead of hardcoded JPY
 export const TransactionListItem = ({ transaction, disallowOpen = false }: Props) => {
   const [open, setOpen] = useState(false);
+  const locale = navigator.language;
   const date = DateTime.fromISO(transaction.date).toFormat("MMM d, yyyy");
+  const { data: meDetail, isLoading: meDetailIsLoading } = useApiV1UserMeDetailGetMeDetailQuery();
+
+  if (!meDetail || meDetailIsLoading) {
+    return <></>;
+  }
+
+  const currency = parseCurrencyEnum(meDetail.default_currency, locale);
+  const formattedAmount = `- ${formatCurrencyValueToLocale(transaction.amount, currency, locale)}`;
 
   const handleOpen = (open: boolean) => () => {
     if (disallowOpen) return;
@@ -27,7 +36,7 @@ export const TransactionListItem = ({ transaction, disallowOpen = false }: Props
             primary={transaction.title}
             secondary={date}
           />
-          <Typography>- {transaction.amount} JPY</Typography>
+          <Typography>{formattedAmount}</Typography>
         </ListItemButton>
       </ListItem>
       <TransactionDetails
