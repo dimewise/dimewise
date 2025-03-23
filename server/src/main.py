a19bdb4dd1088840aca40bb57dea548ddf8a79c9
luke-teo <67887422+lukteo@users.noreply.github.com>
2025-03-23 22:36:57 +0900

    refactor: mdoularize controllers

--- server/src/main.py
+++ server/src/main.py
@@ -3,11 +3,11 @@ from litestar.contrib.sqlalchemy.plugins import SQLAlchemyPlugin
 from litestar.openapi.config import OpenAPIConfig
 from litestar.openapi.plugins import YamlRenderPlugin
 
-from src.categories.controller import CategoryController
+from src.controller.category import CategoryController
+from src.controller.expense import ExpenseController
+from src.controller.user import UserController
 from src.core.database import db_config, provide_db
 from src.core.settings import settings
-from src.expenses.controller import ExpenseController
-from src.users.controller import UserController
 from src.utils.jwt import jwt_auth
 
 
