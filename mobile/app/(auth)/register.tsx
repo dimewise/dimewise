9c712e7ee8377abe31fa3e8611d5def83cee6cb6
luke-teo <67887422+lukteo@users.noreply.github.com>
2025-04-12 22:26:21 +0900

    feat: updated sign up page to properly complete registration process

--- mobile/app/(auth)/register.tsx
+++ mobile/app/(auth)/register.tsx
@@ -6,7 +6,7 @@ import { useMakeGlobalStyles } from "@/hooks/useMakeGlobalStyles";
 import { useAppSelector } from "@/store/store";
 import type { Theme } from "@/style/theme";
 import { Image } from "expo-image";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
 import { useState } from "react";
 import { Alert, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -20,8 +20,9 @@ export default function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [signupComplete, setSignupComplete] = useState(false);
 
-  async function signUpWithEmail() {
+  const signUpWithEmail = async () => {
     if (password === confirmPassword) {
       const { data, error: registerErr } = await register({
         email: email,
@@ -30,63 +31,83 @@ export default function Register() {
 
       // on error, show an alert
       if (registerErr) Alert.alert(registerErr.message);
-      // on successs, notify that email will be sent
-      if (data) Alert.alert("Please check your inbox for email verification!");
+      // on success, show success message and redirect button
+      if (data) setSignupComplete(true);
     } else {
       Alert.alert("Password does not match");
     }
-  }
+  };
+
+  const handleComplete = () => {
+    router.navigate("/login");
+
+    setTimeout(() => setSignupComplete(false), 500);
+  };
 
   return (
     <SafeAreaView style={gstyles.container}>
-      {session.loading && <Text>Loading...</Text>}
       <Image
         style={styles.logo}
         source={require("../../assets/logo/logo_full_colored.svg")}
         contentFit="contain"
       />
-      <Text style={styles.greetings}>Welcome back!</Text>
-      <View style={styles.input}>
-        <Input
-          label="Email"
-          onChangeText={(text) => setEmail(text)}
-          value={email}
-          placeholder="yamada.taro@email.com"
-        />
-        <Input
-          label="Password"
-          onChangeText={(text) => setPassword(text)}
-          value={password}
-          secureTextEntry={true}
-          placeholder="Password"
-        />
-        <Input
-          label="Confirm Password"
-          onChangeText={(text) => setConfirmPassword(text)}
-          value={confirmPassword}
-          secureTextEntry={true}
-          placeholder="Confirm Password"
-        />
-      </View>
-      <View>
-        <Button
-          color="primary"
-          label="Sign Up"
-          disabled={session.loading}
-          onPress={() => signUpWithEmail()}
-        />
-      </View>
-      <View>
-        <Text>
-          Already have an account?&nbsp;
-          <Link
-            style={styles.signUpText}
-            href={"/(auth)/login"}
-          >
-            Sign In
-          </Link>
-        </Text>
-      </View>
+      <Text style={styles.greetings}>Sign Up</Text>
+      {session.loading ? (
+        <Text>Loading...</Text>
+      ) : signupComplete ? (
+        <>
+          <Text>Please check your inbox for email verification!</Text>
+          <Button
+            color="primary"
+            label="Go to Login"
+            onPress={handleComplete}
+          />
+        </>
+      ) : (
+        <>
+          <View style={styles.input}>
+            <Input
+              label="Email"
+              onChangeText={(text) => setEmail(text)}
+              value={email}
+              placeholder="yamada.taro@email.com"
+            />
+            <Input
+              label="Password"
+              onChangeText={(text) => setPassword(text)}
+              value={password}
+              secureTextEntry={true}
+              placeholder="Password"
+            />
+            <Input
+              label="Confirm Password"
+              onChangeText={(text) => setConfirmPassword(text)}
+              value={confirmPassword}
+              secureTextEntry={true}
+              placeholder="Confirm Password"
+            />
+          </View>
+          <View>
+            <Button
+              color="primary"
+              label="Sign Up"
+              disabled={session.loading}
+              onPress={() => signUpWithEmail()}
+            />
+          </View>
+          <View>
+            <Text>
+              Already have an account?&nbsp;
+              <Link
+                style={styles.signUpText}
+                href={"/login"}
+              >
+                Sign In
+              </Link>
+            </Text>
+          </View>
+        </>
+      )}
     </SafeAreaView>
   );
 }
