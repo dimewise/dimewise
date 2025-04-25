6bbaead4e8e1fb850ffc5aef7cf3d4e3d5d9646a
luke-teo <67887422+lukteo@users.noreply.github.com>
2025-04-25 22:28:00 +0900

    fix: missing txs on transactions page

--- mobile/app/(app)/(tabs)/transactions.tsx
+++ mobile/app/(app)/(tabs)/transactions.tsx
@@ -1,6 +1,13 @@
+import {
+  TransactionBottomSheet,
+  type TransactionBottomSheetHandle,
+} from "@/components/TransactionBottomSheet";
 import { TransactionGroup } from "@/components/transactions/TransactionGroup";
 import { useAppTheme } from "@/hooks/useAppTheme";
 import { useMakeGlobalStyles } from "@/hooks/useMakeGlobalStyles";
+import { FakerExpense } from "@/lib/util/faker";
+import type { Expense } from "@/store/api/rtk/server/v1";
+import { useCallback, useRef, useState } from "react";
 import { ScrollView } from "react-native";
 import { ExpandableCalendar } from "react-native-calendars";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -8,6 +15,23 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function Transactions() {
   const theme = useAppTheme();
   const gstyles = useMakeGlobalStyles(theme);
+
+  const [selectedTx, setSelectedTx] = useState<Expense | null>(null);
+
+  const txModalRef = useRef<TransactionBottomSheetHandle>(null);
+  const onPressTransaction = useCallback((tx: Expense) => {
+    setSelectedTx(tx);
+    txModalRef.current?.open();
+  }, []);
+
+  // TODO: Switch to use API call
+  const txs: Expense[] = [
+    FakerExpense,
+    FakerExpense,
+    FakerExpense,
+    FakerExpense,
+    FakerExpense,
+  ];
   return (
     <SafeAreaView
       edges={["top", "left", "right"]}
@@ -25,11 +49,32 @@ export default function Transactions() {
         }}
       />
       <ScrollView>
-        <TransactionGroup />
-        <TransactionGroup />
-        <TransactionGroup />
-        <TransactionGroup />
+        <TransactionGroup
+          txs={txs}
+          onPress={onPressTransaction}
+        />
+        <TransactionGroup
+          txs={txs}
+          onPress={onPressTransaction}
+        />
+        <TransactionGroup
+          txs={txs}
+          onPress={onPressTransaction}
+        />
+        <TransactionGroup
+          txs={txs}
+          onPress={onPressTransaction}
+        />
+        <TransactionGroup
+          txs={txs}
+          onPress={onPressTransaction}
+        />
       </ScrollView>
+      <TransactionBottomSheet
+        ref={txModalRef}
+        tx={selectedTx}
+        setTx={setSelectedTx}
+      />
     </SafeAreaView>
   );
 }
