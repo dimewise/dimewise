ce58bade4c38bdd1785305d736d649583ee99f8f
luke-teo <67887422+lukteo@users.noreply.github.com>
2025-08-25 23:27:50 +0900

    feat: added skeleton for golang web server

---
+++ server/config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"database/sql"
+
+	"github.com/dimewise/dimewise/config/provider"
+
+	"github.com/clerk/clerk-sdk-go/v2/user"
+)
+
+type Config struct {
+	env   *provider.EnvProvider
+	db    *sql.DB
+	clerk *user.Client
+}
+
+func (c *Config) Env() *provider.EnvProvider {
+	return c.env
+}
+
+func (c *Config) DB() *sql.DB {
+	if c.db == nil {
+		c.db = provider.NewDBProvider(c.env)
+	}
+
+	return c.db
+}
+
+func (c *Config) Clerk() *user.Client {
+	if c.clerk == nil {
+		c.clerk = provider.NewClerkProvider(c.env)
+	}
+
+	return c.clerk
+}
+
+func NewConfig() *Config {
+	config := Config{}
+
+	config.env = provider.NewEnvProvider()
+
+	return &config
+}
---
+++ server/internal/server/server.go
@@ -0,0 +1,70 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	clerkhttp "github.com/clerk/clerk-sdk-go/v2/http"
+	"github.com/dimewise/dimewise/config"
+	"github.com/dimewise/dimewise/generated/oapi"
+	"github.com/dimewise/dimewise/internal/server/handler"
+	"github.com/go-chi/chi/v5"
+	chimiddleware "github.com/go-chi/chi/v5/middleware"
+)
+
+type Server struct {
+	router   *chi.Mux
+	portAddr string
+}
+
+func (s *Server) Start() {
+	log.Print("Server listening on " + s.portAddr)
+
+	httpServer := &http.Server{
+		Handler:           s.router,
+		Addr:              s.portAddr,
+		ReadHeaderTimeout: 1000 * time.Second,
+	}
+
+	log.Fatal(httpServer.ListenAndServe())
+}
+
+func (s *Server) Router() *chi.Mux {
+	return s.router
+}
+
+func NewServer(config *config.Config) *Server {
+	h := handler.NewHandler(config)
+	portAddr := fmt.Sprintf(":%s", config.Env().ServerPort())
+
+	r := chi.NewRouter()
+	r.Use(chimiddleware.Logger)
+	r.Use(chimiddleware.Recoverer)
+
+	/* -- Health -- */
+	r.Get("/api/v1/health", h.GetHealth)
+
+	/* -- Private OAPI -- */
+	r.Group(func(r chi.Router) {
+		baseURL := "/api/v1"
+
+		// clerk auth middleware
+		r.Use(clerkhttp.RequireHeaderAuthorization())
+
+		// TODO: add StrictHTTPServerOptions
+		strictHandler := oapi.NewStrictHandlerWithOptions(
+			h,
+			[]oapi.StrictMiddlewareFunc{},
+			oapi.StrictHTTPServerOptions{},
+		)
+
+		oapi.HandlerFromMuxWithBaseURL(strictHandler, r, baseURL)
+	})
+
+	return &Server{
+		router:   r,
+		portAddr: portAddr,
+	}
+}
