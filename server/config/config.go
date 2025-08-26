3c182561756393e7f089a374dfffb55017910c3b
luke-teo <67887422+lukteo@users.noreply.github.com>
2025-08-26 23:05:46 +0900

    feat: configured slog as default logger

--- server/config/config.go
+++ server/config/config.go
@@ -2,28 +2,18 @@ package config
 
 import (
 	"database/sql"
-
-	"github.com/dimewise/dimewise/config/provider"
+	"log/slog"
 
 	"github.com/clerk/clerk-sdk-go/v2/user"
+
+	"github.com/dimewise/dimewise/config/provider"
 )
 
 type Config struct {
-	env   *provider.EnvProvider
-	db    *sql.DB
-	clerk *user.Client
-}
-
-func (c *Config) Env() *provider.EnvProvider {
-	return c.env
-}
-
-func (c *Config) DB() *sql.DB {
-	if c.db == nil {
-		c.db = provider.NewDBProvider(c.env)
-	}
-
-	return c.db
+	clerk  *user.Client
+	db     *sql.DB
+	env    *provider.EnvProvider
+	logger *slog.Logger
 }
 
 func (c *Config) Clerk() *user.Client {
@@ -34,10 +24,31 @@ func (c *Config) Clerk() *user.Client {
 	return c.clerk
 }
 
+func (c *Config) DB() *sql.DB {
+	if c.db == nil {
+		c.db = provider.NewDBProvider(c.env)
+	}
+
+	return c.db
+}
+
+func (c *Config) Env() *provider.EnvProvider {
+	return c.env
+}
+
+func (c *Config) Logger() *slog.Logger {
+	if c.logger == nil {
+		c.logger = provider.NewLoggerProvider(c.env)
+	}
+
+	return c.logger
+}
+
 func NewConfig() *Config {
 	config := Config{}
 
 	config.env = provider.NewEnvProvider()
+	config.logger = provider.NewLoggerProvider(config.env)
 
 	return &config
 }
