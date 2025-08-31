d14ebfd7ef573ca14202fe90df0e47fcb94b2427
luke-teo <67887422+lukteo@users.noreply.github.com>
2025-08-31 23:51:06 +0900

    feat: added orval integration for gen-openapi command + zod

--- makefiles/generator.mk
+++ makefiles/generator.mk
@@ -2,6 +2,8 @@
 
 .PHONY: gen-openapi
 gen-openapi: ## Generates code based on OpenAPI specification
+	@echo "Generating mobile code from ~/openapi.yaml"
+	@cd ./mobile && bun run gen-openapi
 	@echo "Generating server code from ~/openapi.yaml"
 	@cd ./server && go tool oapi-codegen -config ./generated/oapi/config.yaml ../openapi.yaml
 	@echo "OpenAPI generation complete"
