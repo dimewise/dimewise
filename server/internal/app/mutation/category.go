2296fd0a83efe049b999f4f028d4050390b344ce
luke-teo <67887422+lukteo@users.noreply.github.com>
2025-09-03 22:42:55 +0900

    feat: added delete category logic

--- server/internal/app/mutation/category.go
+++ server/internal/app/mutation/category.go
@@ -4,7 +4,9 @@ import (
 	"context"
 
 	"github.com/go-errors/errors"
+	"github.com/go-jet/jet/v2/postgres"
 	"github.com/go-jet/jet/v2/qrm"
+	"github.com/google/uuid"
 
 	"github.com/dimewise/dimewise/generated/dimewise/public/model"
 	"github.com/dimewise/dimewise/generated/dimewise/public/table"
@@ -26,4 +28,32 @@ func InsertCategoryByModel(
 	}
 
 	return &dest, nil
+}
+
+func DeleteCategoryByID(
+	ctx context.Context,
+	db qrm.DB,
+	userID uuid.UUID,
+	categoryID uuid.UUID,
+) error {
+	tbl := table.Category
+
+	stmt := tbl.DELETE().
+		WHERE(tbl.UserID.EQ(postgres.UUID(userID)).AND(tbl.ID.EQ(postgres.UUID(categoryID))))
+
+	res, err := stmt.ExecContext(ctx, db)
+	if err != nil {
+		return errors.Errorf("failed to delete category by id: %w", err)
+	}
+
+	affectedCount, err := res.RowsAffected()
+	if err != nil {
+		return errors.Errorf("failed to get rows affected after delete category: %w", err)
+	}
+
+	if affectedCount != 1 {
+		return errors.Errorf("expected to delete 1 row, got %d", affectedCount)
+	}
+
+	return nil
 }
