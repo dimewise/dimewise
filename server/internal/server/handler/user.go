929c2066be4c418770fd4a982b0d9526feb8c9bd
luke-teo <67887422+lukteo@users.noreply.github.com>
2025-09-10 22:57:39 +0900

    feat: added user handler and service logic

--- server/internal/server/handler/user.go
+++ server/internal/server/handler/user.go
@@ -3,26 +3,90 @@ package handler
 import (
 	"context"
 
+	"github.com/go-errors/errors"
+
 	"github.com/dimewise/dimewise/generated/oapi"
+	"github.com/dimewise/dimewise/internal/server/service"
+	usersvc "github.com/dimewise/dimewise/internal/server/service/user"
 )
 
 func (h *Handler) PostMeUser(
 	ctx context.Context,
 	req oapi.PostMeUserRequestObject,
 ) (oapi.PostMeUserResponseObject, error) {
-	return oapi.PostMeUser201JSONResponse{}, nil
+	res, err := usersvc.CreateUser(ctx, h.config, *req.Body)
+	if err != nil {
+		var se *service.Error
+		if errors.As(err, &se) {
+			switch se.Code {
+			case service.ErrCodeBadRequest:
+				return oapi.PostMeUser400JSONResponse{Code: string(se.Code), Success: false}, nil
+			case service.ErrCodeUnauthorized:
+				fallthrough
+			case service.ErrCodeForbidden:
+				fallthrough
+			case service.ErrCodeNotFound:
+				fallthrough
+			default:
+				return nil, err
+			}
+		}
+		return nil, err
+	}
+
+	return oapi.PostMeUser201JSONResponse(*res), nil
 }
 
 func (h *Handler) GetMeUser(
 	ctx context.Context,
 	req oapi.GetMeUserRequestObject,
 ) (oapi.GetMeUserResponseObject, error) {
-	return oapi.GetMeUser200JSONResponse{}, nil
+	res, err := usersvc.GetUser(ctx, h.config)
+	if err != nil {
+		var se *service.Error
+		if errors.As(err, &se) {
+			switch se.Code {
+			case service.ErrCodeBadRequest:
+				fallthrough
+			case service.ErrCodeUnauthorized:
+				return oapi.GetMeUser401JSONResponse{Code: string(se.Code), Success: false}, nil
+			case service.ErrCodeForbidden:
+				fallthrough
+			case service.ErrCodeNotFound:
+				return oapi.GetMeUser404JSONResponse{Code: string(se.Code), Success: false}, nil
+			default:
+				return nil, err
+			}
+		}
+		return nil, err
+	}
+
+	return oapi.GetMeUser200JSONResponse(*res), nil
 }
 
 func (h *Handler) PutMeUser(
 	ctx context.Context,
 	req oapi.PutMeUserRequestObject,
 ) (oapi.PutMeUserResponseObject, error) {
-	return oapi.PutMeUser200JSONResponse{}, nil
+	res, err := usersvc.UpdateUser(ctx, h.config, *req.Body)
+	if err != nil {
+		var se *service.Error
+		if errors.As(err, &se) {
+			switch se.Code {
+			case service.ErrCodeBadRequest:
+				return oapi.PutMeUser400JSONResponse{Code: string(se.Code), Success: false}, nil
+			case service.ErrCodeUnauthorized:
+				return oapi.PutMeUser401JSONResponse{Code: string(se.Code), Success: false}, nil
+			case service.ErrCodeForbidden:
+				fallthrough
+			case service.ErrCodeNotFound:
+				fallthrough
+			default:
+				return nil, err
+			}
+		}
+		return nil, err
+	}
+
+	return oapi.PutMeUser200JSONResponse(*res), nil
 }
