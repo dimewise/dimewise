38b13590511abb518e2f6a94042ddc8551662fd8
luke-teo <67887422+lukteo@users.noreply.github.com>
2025-09-10 22:58:02 +0900

    feat: added expense handler and service logic

---
+++ server/internal/server/service/expense/get_expense_by_id.go
@@ -0,0 +1,48 @@
+package expensesvc
+
+import (
+	"context"
+
+	"github.com/go-errors/errors"
+	"github.com/google/uuid"
+
+	"github.com/dimewise/dimewise/config"
+	"github.com/dimewise/dimewise/generated/oapi"
+	"github.com/dimewise/dimewise/internal/app/dto"
+	"github.com/dimewise/dimewise/internal/app/repository"
+	"github.com/dimewise/dimewise/internal/server/middleware"
+	"github.com/dimewise/dimewise/internal/server/service"
+)
+
+func GetExpenseByID(
+	ctx context.Context,
+	c *config.Config,
+	targetExpenseID uuid.UUID,
+) (*oapi.ExpenseWithDetails, error) {
+	user, ok := middleware.GetAppUserFromContext(ctx)
+	if !ok {
+		return nil, service.NewError(
+			service.ErrCodeUnauthorized,
+			"authenticated user not found in context",
+			errors.Errorf("authenticated user not found in context"),
+		)
+	}
+
+	expenseFull, err := repository.GetExpenseFullByID(ctx, c.DB(), user.ID, targetExpenseID)
+	if err != nil {
+		var re *repository.Error
+		if errors.As(err, &re) {
+			switch re.Code {
+			case repository.ErrCodeNotFound:
+				return nil, service.NewError(service.ErrCodeNotFound, "expense not found", err)
+			default:
+				return nil, err
+			}
+		}
+		return nil, err
+	}
+
+	oapiExpense := dto.TransformExpenseFullToOAPIExpenseWithDetails(*expenseFull)
+
+	return &oapiExpense, nil
+}
