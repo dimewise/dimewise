b4d732c27da3506bb89c920ca439ad78844ddb33
luke-teo <67887422+lukteo@users.noreply.github.com>
2025-09-16 20:58:24 +0900

    feat: setup redux and toolkit

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,11 +1,15 @@
 import { ClerkProvider } from '@clerk/clerk-expo';
 import { tokenCache } from '@clerk/clerk-expo/token-cache';
 import { Slot } from 'expo-router';
+import { Provider } from 'react-redux';
+import { store } from '@/store/store';
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache}>
-      <Slot />
-    </ClerkProvider>
+    <Provider store={store}>
+      <ClerkProvider tokenCache={tokenCache}>
+        <Slot />
+      </ClerkProvider>
+    </Provider>
   );
 }
