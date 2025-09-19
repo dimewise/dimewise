f65fc2b501b32486c4ba5828a2dde0e195fdaa60
luke-teo <67887422+lukteo@users.noreply.github.com>
2025-09-19 22:14:54 +0900

    fix: remove redundant action prop
    Clerk treats sso login and register as one unified flow. Unless there is
    a need for MFA in the future, there is no need to have that prop for now

--- mobile/app/(auth)/sign-up.tsx
+++ mobile/app/(auth)/sign-up.tsx
@@ -170,7 +170,6 @@ export default function SignUpScreen() {
                 <FormSSOButton
                   key={name}
                   social={name}
-                  action="signup"
                 />
               ))}
             </View>
