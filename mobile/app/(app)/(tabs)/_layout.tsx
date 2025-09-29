de11cea8aa694b707ff603cafcea55cbbb435177
luke-teo <67887422+lukteo@users.noreply.github.com>
2025-09-29 22:55:58 +0900

    feat: added tabs for app layer

---
+++ mobile/app/(app)/(tabs)/_layout.tsx
@@ -0,0 +1,62 @@
+import Octicons from '@expo/vector-icons/Octicons';
+import { Tabs } from 'expo-router';
+import { useTranslation } from 'react-i18next';
+import { colors } from '@/theme/colors';
+
+export default function TabLayout() {
+  const { t } = useTranslation();
+
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: colors.primary,
+        tabBarInactiveTintColor: colors.textPrimary,
+        tabBarStyle: {
+          backgroundColor: colors.backgroundDefault,
+          borderTopColor: colors.primary,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: t('nav_overview'),
+          tabBarIcon: ({ color, size }) => (
+            <Octicons
+              name="project"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="transactions"
+        options={{
+          title: t('nav_transactions'),
+          tabBarIcon: ({ color, size }) => (
+            <Octicons
+              name="arrow-switch"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: t('nav_settings'),
+          tabBarIcon: ({ color, size }) => (
+            <Octicons
+              name="gear"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
