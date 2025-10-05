707af17284d5f725e385a62d7e8c5c5b9b4a2e14
luke-teo <67887422+lukteo@users.noreply.github.com>
2025-10-05 22:42:41 +0900

    feat: added reactotron for debugging

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,3 +1,7 @@
+if (__DEV__) {
+  require('../ReactotronConfig');
+}
+
 import { ClerkProvider } from '@clerk/clerk-expo';
 import { tokenCache } from '@clerk/clerk-expo/token-cache';
 import { Slot } from 'expo-router';
