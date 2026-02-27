4ed8be16be25c4ed4a91a52863513d5ead8ac64f
luke-teo <67887422+lukteo@users.noreply.github.com>
2026-02-27 23:06:36 +0900

    fix: top area padding

--- client/src/components/Layout/PublicLayout.tsx
+++ client/src/components/Layout/PublicLayout.tsx
@@ -7,7 +7,7 @@ export const PublicLayout = () => {
 	const navigate = useNavigate();
 
 	return (
-		<div className="min-h-screen bg-background">
+		<div className="min-h-screen bg-background pt-[env(safe-area-inset-top)]">
 			<Toaster
 				position="top-center"
 				toastOptions={{
